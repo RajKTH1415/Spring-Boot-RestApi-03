c96e10c444d8c2f38d232435831919855f3b74c2
Deepak kumar <deepakmehta2401@gmail.com>
2025-07-02 15:00:02 +0530

    method post to get

--- src/main/java/com/springboot/controller/EcoController.java
+++ src/main/java/com/springboot/controller/EcoController.java
@@ -1,6 +1,7 @@
 package com.springboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/eco")
 public class EcoController {
 
-    @PostMapping
+    @GetMapping
     public  String ecoController(){
         return "Hello world";
     }
